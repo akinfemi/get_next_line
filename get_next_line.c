8df257388512cb17c05813b2fd71ea4bee95fcf6
Akinfemi Akin-Aluko <aakin-al@e1z2r7p5.42.us.org>
2017-04-04 18:39:13 -0700

    almost done

--- get_next_line.c
+++ get_next_line.c
@@ -1,33 +1,62 @@
 #include "get_next_line.h"
-#include <stdio.h>
 
-int		get_next_line(const int fd, char **line)
+void		init(char **str)
 {
-	static	char *str;
-	char	*buf;
-
-	if (fd < 0)
-		return (0);
-	while(read(fd, buf, BUF_SIZE))
-	{
-		if (ft_strchr(buf, '\n'))
-	}
+	*str = (char *)ft_memalloc(sizeof(char) * 1001);
 }
 
-int 	main(int ac, char **ag)
+char		*read_next(const int fd, int buf_size)
 {
-	int		buf_size = 1;
-	char 	*buf = (char *)malloc(sizeof(char) * buf_size + 1);
-	int fd = open(ag[1], O_RDONLY);
-	ac = read(fd, buf, buf_size);
-	buf[buf_size] = '\0';
-	while(ac != 0)
+	int		rd;
+	char	*temp;
+
+	temp = (char *)ft_memalloc(sizeof(char) * BUF_SIZE + 1);
+	rd = read(fd, temp, buf_size);
+	if (rd > 0)
+		return (temp);
+	else
+		return (0);
+}
+
+void		fill_line(char **line, char **str)
+{
+	size_t		len;
+	char		*temp;
+
+	temp = ft_strchr(*str, '\n');
+	len = ft_strlen(*str) - ft_strlen(temp);
+	*line = (char *)ft_memalloc(sizeof(char) * len + 1);
+	*line = ft_strncpy(*line, *str, len);
+	*str = temp+1;
+}
+
+int			get_next_line(const int fd, char **line)
+{
+	static char	*str = NULL;
+	char	*nl;
+	char	*temp;
+   
+	if (!str)
 	{
-		printf("AC: %d\nBuf: %s\n", ac, buf);
-		buf_size*=2;
-		buf = (char *)malloc(sizeof(char) * buf_size + 1);
-		buf[buf_size] = '\0';
-		ac = read(fd, buf, buf_size);
+		init(&str);
+		temp = read_next(fd, BUF_SIZE);
+		if (!temp)
+			return (0);
+		str = ft_strcpy(str, temp);
 	}
-	return (0);
+	nl = ft_strchr(str, '\n');
+	if (nl && *nl == '\n')
+	{
+		fill_line(line, &str);
+		return (1);
+	}
+	if (nl == NULL)
+	{
+		temp = read_next(fd, BUF_SIZE);
+		if (!temp)
+			return (0);
+		str = ft_strcat(str, temp);
+		return (get_next_line(fd, line));
+	}
+	return (0);	
 }
